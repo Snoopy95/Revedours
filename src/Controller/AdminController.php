a825b7bc0fca780877244e3772d10cf0f003cf0f
snoopy95 <joebart95@free.fr>
2020-03-25 18:19:47 +0100

    le session start qui bloc

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-session_start();
+// session_start();
 
 class AdminController extends AbstractController
 {
