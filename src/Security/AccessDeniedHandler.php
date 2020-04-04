ee0de70a1def0547197f404971fbf5244398bfe8
snoopy95 <joebart95@free.fr>
2020-04-05 01:46:48 +0200

    PAGE 403 OK

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        $content='Acces Refuser !!!  Il faut retourné sur le droit chemin.';
+
+        return new Response($content, 403);
+    }
+}
